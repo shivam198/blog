d0d2b353cb6070a906112cffe6d79a50f5a6db0d
Shivam Pandey <shivam.pandey@thoughtworks.com>
2020-07-02 00:09:24 +0530

    Initial Commit

---
+++ src/main/java/com/example/blog/controllers/BlogController.java
@@ -0,0 +1,15 @@
+package com.example.blog.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogController {
+
+    @GetMapping("")
+    public String blogs(){
+        String blog = "This is my first blog!! Wish me luck";
+        return blog;
+    }
+}
---
+++ src/test/java/com/example/blog/controllers/BlogControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.blog.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlogControllerTest {
+
+}
