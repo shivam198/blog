87733b619bed8dcb37d776208c9102f28ce493d6
Shivam Pandey <shivam.pandey@thoughtworks.com>
2020-07-09 19:53:33 +0530

    Query Parameters

--- src/main/java/com/example/blog/controllers/BlogController.java
+++ src/main/java/com/example/blog/controllers/BlogController.java
@@ -2,18 +2,35 @@ package com.example.blog.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.blog.models.BlogModel;
 import static com.example.blog.models.BlogModel.createBlogs;
 
 @RestController
+@RequestMapping("/blogs")
 public class BlogController {
 
-    @GetMapping("/")
+    @GetMapping()
     public ResponseEntity<BlogModel[]> blogs(){
         BlogModel[] blogs = createBlogs();
         return new ResponseEntity<>(blogs, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/testing/{author}")
+    public String getUser(@PathVariable String author){
+        return "Hey!!! My name is " + author;
+    }
+
+    @GetMapping("/testing")
+    public String getUsers(@RequestParam(value = "author")String author){
+        return "Hey!!! My name is" + author;
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<BlogModel> getBlog(@RequestParam(value = "id") int id){
+        BlogModel[] blogs = createBlogs();
+        BlogModel blog = blogs[id-1];
+        return new ResponseEntity<>(blog, HttpStatus.OK);
     }
 
 }
